4845ec341ee7bb543b9a91c18d83b7921ec498c6
SnakeWhisperer <snake@whisperer.com>
2021-04-19 11:07:22 -0400

    Reorganized the directory to contain sub-directories so that the file names do not conflict. Every sub-directory now has a descriptive name for what it contains and the task(s) involved.

---
+++ verify-installation/main.js
@@ -0,0 +1,10 @@
+/* 
+Hello, World! program in node.js As gotten from
+https://www.tutorialspoint.com/nodejs/nodejs_environment_setup.htm
+to verify installation.
+
+This does nothing but log 'Hello, World!'.
+
+*/
+
+console.log("Hello, World!")
